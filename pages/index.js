47b655212aab9f02533e1d6590cb186c43578656
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2022-01-06 13:59:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>hey !! idiots</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
