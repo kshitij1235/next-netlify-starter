52459460b2a6507c764de5db3c2450a2d175d2e6
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2022-01-06 14:01:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my worst website!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          listeren here you  <code>pages/index.js</code>
         </p>
       </main>
 
